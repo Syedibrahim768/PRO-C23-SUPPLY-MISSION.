da478fb1238e2142e98e23c7bf3770c476d48c89
Syedibrahim768 <78018536+Syedibrahim768@users.noreply.github.com>
2021-05-01 17:08:36 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -73,7 +73,19 @@ function draw() {
  
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
+  if(keyCode===LEFT_ARROW) {
+	helicopterSprite.x=helicopterSprite.x-20;
+	Matter.Body.translate(packageBody,{x:-20,y:0})
+  }
 
+  if(keyCode===RIGHT_ARROW) {
+	helicopterSprite.x=helicopterSprite.x+20;
+	Matter.Body.translate(packageBody,{x:+20,y:0})
+  } 
+  if(keyCode===DOWN_ARROW) {
+	packageSprite.y=packageSprite.y+20;
+	Matter.Body.setStatic(packageBody,false);
+  } 
   
   drawSprites();
   
